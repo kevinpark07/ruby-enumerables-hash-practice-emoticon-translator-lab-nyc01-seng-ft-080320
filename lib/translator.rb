735ee20c97b847db5dc7e36691cf95e1dd4db40c
Kevin Park <kevin.park07@yahoo.com>
2020-07-15 16:16:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,8 +19,8 @@ def get_english_meaning(path, emoticon)
   emoticon_meaning = ""
 
   emoticon_hash.each do |key, language|
-    if emoticon == emoticon_hash[key][:japanese]
-     emoticon_meaning = emoticon_hash[key]
+    if emoticon == emoticon_hash[:key][:japanese]
+     emoticon_meaning = emoticon_hash[:key]
     else
       "Sorry, that emoticon was not found"
   end
